5999bfdadb9af4762c01ddf60bc9b3b539223ab4
Joe Doe <joe_doe@gmail.com>
2018-05-04 11:32:44 +0700

    update

--- web/api/get_update.php
+++ web/api/get_update.php
@@ -1,28 +1,5 @@
 <?php
 header ( "Content-type: application/json; charset=utf-8" );
-$result = file_get_content('./update.json');
-// $result = '{
-// 	"status": "ok",
-//     "status_message": "Query was successful",
-//     "data" : {
-// 	"mobile":[
-// 		{
-
-// 		"version":"1.0.2",
-// 		"link":""
-// 		}],
-
-// 	"tv":{
-// 		"version":"1.0.2",
-// 		"link":""
-// 		},
-
-// 	"tablet":{
-// 		"version":"1.0.2",
-// 		"link":""
-// 		},
-// 	"genres":"action;adventure;animation;biography;comedy;crime;documentary;drama;family;fantasy;history;horror;musical;mystery;news;romance;sci-fi;sport;thriller;war;western"
-// 	}
-// }';
+$result = file_get_contents('./update.json');
 echo $result;
 ?>
