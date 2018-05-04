b34c8f653ce36a28a96161a2ea9e816536917df1
Joe Doe <joe_doe@gmail.com>
2018-05-04 10:05:35 +0700

    update

--- web/api/get_update.php
+++ web/api/get_update.php
@@ -1,25 +1,28 @@
 <?php
-header("Content-type: application/json; charset=utf-8");
-$result = '{
-	"status": "ok",
-    "status_message": "Query was successful",
-    "data" : {
-	"mobile":{
-		"version":"1.0.2",
-		"link":""
-		},
+header ( "Content-type: application/json; charset=utf-8" );
+$result = file_get_content('./update.json');
+// $result = '{
+// 	"status": "ok",
+//     "status_message": "Query was successful",
+//     "data" : {
+// 	"mobile":[
+// 		{
 
-	"tv":{
-		"version":"1.0.2",
-		"link":""
-		},
+// 		"version":"1.0.2",
+// 		"link":""
+// 		}],
 
-	"tablet":{
-		"version":"1.0.2",
-		"link":""
-		},
-	"genres":"action;adventure;animation;biography;comedy;crime;documentary;drama;family;fantasy;history;horror;musical;mystery;news;romance;sci-fi;sport;thriller;war;western"
-	}
-}';
+// 	"tv":{
+// 		"version":"1.0.2",
+// 		"link":""
+// 		},
+
+// 	"tablet":{
+// 		"version":"1.0.2",
+// 		"link":""
+// 		},
+// 	"genres":"action;adventure;animation;biography;comedy;crime;documentary;drama;family;fantasy;history;horror;musical;mystery;news;romance;sci-fi;sport;thriller;war;western"
+// 	}
+// }';
 echo $result;
 ?>
